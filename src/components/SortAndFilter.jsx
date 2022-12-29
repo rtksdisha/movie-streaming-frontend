64a7f5c83862a61370d081b9ff6fffb945da4faf
Disha Nath <dnath@uoguelph.ca>
2022-12-28 20:27:17 -0500

    add ascending movie filter

--- src/components/SortAndFilter.jsx
+++ src/components/SortAndFilter.jsx
@@ -4,6 +4,32 @@ import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
 import React, { useCallback, useEffect } from "react";
 
+function moviesFilter(value, filteredMovies) {
+  if (filteredMovies) {
+    console.log(filteredMovies);
+    const field = value.split("-")[0];
+    const direction = value.split("-")[1];
+
+    if (direction == "descending") {
+      return filteredMovies.sort((a, b) => {
+        return a[field][0] < b[field][0]
+          ? 1
+          : a[field][0] > b[field][0]
+          ? -1
+          : 0;
+      });
+    } else {
+      return filteredMovies.sort((a, b) => {
+        return a[field][0] < b[field][0]
+          ? -1
+          : a[field][0] > b[field][0]
+          ? 1
+          : 0;
+      });
+    }
+  }
+}
+
 export default function SortAndFilter({ filteredMovies, setFilteredMovies }) {
   const [filter, setFilter] = React.useState("");
   const [sort, setSort] = React.useState("");
@@ -11,17 +37,12 @@ export default function SortAndFilter({ filteredMovies, setFilteredMovies }) {
   const handleFilter = useCallback((event) => {
     const value = event.target.value;
 
-    const sorted = filteredMovies.sort((a, b) => {
-      return a[value][0] < b[value][0] ? 1 : a[value][0] > b[value][0] ? -1 : 0;
-    });
-
-    setFilteredMovies(sorted);
-    console.log(filteredMovies);
+    setFilteredMovies(moviesFilter(value, filteredMovies));
     setFilter(event.target.value);
   });
 
   const handleSort = (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
     setSort(event.target.value);
   };
 
@@ -39,10 +60,10 @@ export default function SortAndFilter({ filteredMovies, setFilteredMovies }) {
           <MenuItem value="">
             <em>Choose a Genre</em>
           </MenuItem>
-          <MenuItem value={'horror'}>Horror</MenuItem>
-          <MenuItem value={'comedy'}>Comedy</MenuItem>
-          <MenuItem value={'action'}>Action</MenuItem>
-          <MenuItem value={'anime'}>Mystery</MenuItem>
+          <MenuItem value={"horror"}>Horror</MenuItem>
+          <MenuItem value={"comedy"}>Comedy</MenuItem>
+          <MenuItem value={"action"}>Action</MenuItem>
+          <MenuItem value={"mystery"}>Mystery</MenuItem>
         </Select>
       </FormControl>
 
@@ -58,8 +79,16 @@ export default function SortAndFilter({ filteredMovies, setFilteredMovies }) {
           <MenuItem value="">
             <em>Filter By</em>
           </MenuItem>
-          <MenuItem value={"name"}>Name in Descending Order</MenuItem>
-          <MenuItem value={"releaseDate"}>Release Date</MenuItem>
+          <MenuItem value={"name-ascending"}>Name in Ascending Order</MenuItem>
+          <MenuItem value={"name-descending"}>
+            Name in Descending Order
+          </MenuItem>
+          <MenuItem value={"releaseDate-ascending"}>
+            Release Date in Ascending
+          </MenuItem>
+          <MenuItem value={"releaseDate-descending"}>
+            Release Date in Descending
+          </MenuItem>
         </Select>
       </FormControl>
     </>
